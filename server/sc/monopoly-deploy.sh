cbbac80416394a410dff46857b789a76f93aa5d3
dc <dc3@pikkle.com>
2011-10-09 15:48:49 -0700

    add version to display

--- server/sc/monopoly-deploy.sh
+++ server/sc/monopoly-deploy.sh
@@ -1,3 +1,4 @@
-ssh root@173.255.251.200
-cd frennzy
-git pull
+set -x
+ssh root@173.255.251.200 'cd frennzy; git pull'
+ssh root@173.255.251.200 'pkill node'
+ssh root@173.255.251.200 'cd frennzy/server && nohup /root/local/node/bin/node app.js > log/node.log ' &
